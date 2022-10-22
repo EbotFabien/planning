bb3921f75838fd90b2f6e626fb4b6a6eaeb232d9
Ebot Fabien <touchone0001@gmail.com>
2022-10-22 07:24:30 +0100

    done

--- Flask_app/project/app/entity/comment/routes.py
+++ Flask_app/project/app/entity/comment/routes.py
@@ -17,15 +17,44 @@ omment=Blueprint('comment',__name__)
 
 @omment.route('/all/comment/', methods=['GET'])
 def commen():
-    ide=request.get('rdv')
+    ide=request.args.get('rdv')
     commen=comment.query.filter_by(rdv_id=ide).all()
+    all_=[]
+    for i in commen:
+        json={
+            "id":i.id,
+            "user_id":i.user_id,
+            "rdv_id":i.rdv_id,
+            "contenu":i.contenu,
+            "date":i.date
+        }
+        all_.append(json)
 
-    return jsonify({"comment": commen}), 200
+    return jsonify({"comment":all_}), 200
 
 @omment.route('/<int:ide>/comment/', methods=['POST'])
 def commen_spe(ide):
 
     commen=comment.query.filter_by(id=ide).first()
-
+    all_=[]
+    json={
+            "id":commen.id,
+            "user_id":commen.user_id,
+            "rdv_id":commen.rdv_id,
+            "contenu":commen.contenu,
+            "date":commen.date
+        }
+    all_.append(json)
     return jsonify({"comment": commen}), 200
+
+@omment.route('/make/comment/', methods=['POST'])
+def commen_make():
+    rdv=request.json['rdv']
+    user=request.json['user']
+    comme=request.json['comment']
+    commen=comment(user_id=user,rdv_id=rdv,contenu=comme)
+    db.session.add(commen)
+    db.session.commit()
+
+    return jsonify({"status": "comment sent"}), 200
 
