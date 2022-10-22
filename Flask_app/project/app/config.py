6bbe6b2c5a37b00f9bc6cb788e53e281e7e5fb23
Ebot Fabien <touchone0001@gmail.com>
2022-10-22 09:24:06 +0100

    done

--- Flask_app/project/app/config.py
+++ Flask_app/project/app/config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
     SECRET_KEY='FABIENCLASSIC'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///cmd.db'
@@ -6,6 +8,8 @@ class Config:
     MAIL_USE_TLS =True
     MAIL_USERNAME = 'noreply@amexpert.pro'
     MAIL_PASSWORD = 'TooR123$'
+    UPLOAD_FOLDER=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'doc')
+    UPLOAD_FOLDER=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'img')
 
 
 
