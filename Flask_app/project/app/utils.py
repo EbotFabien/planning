a2e1cf0dacc51a7af319978f65ca043262c01701
Ebot Fabien <touchone0001@gmail.com>
2023-01-03 07:05:32 +0100

    done

--- Flask_app/project/app/utils.py
+++ Flask_app/project/app/utils.py
@@ -36,7 +36,7 @@ def doc():
         comme=json['route']
         comment=json['comment']
         entities=(user,rdv,comme,tp,comment)
-        cursorObj.execute('INSERT INTO documen(user_id, rdv_id,route,Type,comment) VALUES(?, ?, ?,?,?)', entities)
+        cursorObj.execute('INSERT INTO document(user_id, rdv_id,route,Type,comment) VALUES(?, ?, ?,?,?)', entities)
         con.commit()
     
     cursorObj.close()
@@ -44,35 +44,39 @@ def doc():
         
 
 
-'''def pic():
-    with app.app_context():
-        loc="/work/www/cmd/Flask_app/project/app/static/appointment_photos.xls"
-        wb = xlrd.open_workbook(loc)
-        sheet = wb.sheet_by_index(0)
-        
-        sheet.cell_value(0,0)
-        for i in range(0,141):
-            name=sheet.row_values(i+1)
-            inv=name[4][::-1]
-            url=inv[0:inv.index('/')]
-            url="/work/fichiers/appointments/photos/" + url[::-1]
-            json={
-                'rdv':int(name[1]),
-                'type':'Photos',
-                'route':url,
-                'comment':name[3],
-                'user':int(name[2]),
-            }
-            user=json['user']
-            #check=requests.get("http://195.15.218.172/manager_app/user/"+str(user), headers={"Authorization":request.headers["Authorization"]})
-            #if #check.json()['id']:
-            rdv=json['rdv']
-            tp=json['type']
-            comme=json['route']
-            comment=json['comment']
-            commen=document(user_id=user,rdv_id=rdv,route=comme,Type=tp,comment=comment)
-            db.session.add(commen)
-            db.session.commit()
+def pic():
+    #with app.app_context():
+    loc="/work/www/cmd/Flask_app/project/app/static/appointment_photos.xls"
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    
+    sheet.cell_value(0,0)
+    for i in range(0,141):
+        name=sheet.row_values(i+1)
+        inv=name[4][::-1]
+        url=inv[0:inv.index('/')]
+        url="/work/fichiers/appointments/photos/" + url[::-1]
+        json={
+            'rdv':int(name[1]),
+            'type':'Photos',
+            'route':url,
+            'comment':name[3],
+            'user':int(name[2]),
+        }
+        user=json['user']
+        #check=requests.get("http://195.15.218.172/manager_app/user/"+str(user), headers={"Authorization":request.headers["Authorization"]})
+        #if #check.json()['id']:
+        rdv=json['rdv']
+        tp=json['type']
+        comme=json['route']
+        comment=json['comment']
+        entities=(user,rdv,comme,tp,comment)
+        cursorObj.execute('INSERT INTO document(user_id, rdv_id,route,Type,comment) VALUES(?, ?, ?,?,?)', entities)
+        con.commit()
+        print(i)
+    
+    cursorObj.close()
+    con.close()
 
 def comment():
     with app.app_context():
@@ -93,8 +97,15 @@ def comment():
             #if #check.json()['id']:
             rdv=request.json['rdv']
             comme=request.json['comment']
-            commen=comment(user_id=user,rdv_id=rdv,contenu=comme)
-            db.session.add(commen)
-            db.session.commit()'''
+            entities=(user,rdv,comme)
+            cursorObj.execute('INSERT INTO comment(user_id, rdv_id,contenu) VALUES(?, ?, ?)', entities)
+            con.commit()
+            print(i)
+    
+    cursorObj.close()
+    con.close()
 
-doc()
+pic()
+print("first step")
+comment()
+print(23)
