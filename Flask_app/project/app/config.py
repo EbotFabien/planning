af3926e8051aa029ff1fdc77de5c48f995803d9b
Ebot Fabien <touchone0001@gmail.com>
2023-01-08 10:04:22 +0100

    done

--- Flask_app/project/app/config.py
+++ Flask_app/project/app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY='FABIENCLASSIC'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///cmd7.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///cmd8.db'
     MAIL_SERVER ='mail.infomaniak.ch'
     MAIL_PORT = 587
     MAIL_USE_TLS =True
